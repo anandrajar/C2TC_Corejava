03ae5c922d4ae189e18d9f55d04b54a0b6d5886f
aspire <aspire@aspire-PC>
2022-05-26 20:14:23 +0530

    first commit

---
+++ corejava/src/abc/addition.java
@@ -0,0 +1,16 @@
+package abc;
+
+public class addition {
+	public int addtwonumbers(int a,int b) {
+		return a+b;
+}
+class b
+{}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		addition ob1=new addition();
+		ob1.addtwonumbers(2,3);
+
+	}
+
+}
---
+++ corejava/src/corejava_package/polyExample.java
@@ -0,0 +1,10 @@
+package corejava_package;
+
+public class polyExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
