ad9c57e28d5fddebee6eb7afe157343d83738865
aspire <aspire@aspire-PC>
2022-05-26 20:18:19 +0530

    first commit

---
+++ corejava/src/xyz/test.java
@@ -0,0 +1,15 @@
+package xyz;
+
+import abc.addition;
+
+public class test {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		addition ob1=new addition();
+		System.out.println(ob1.addtwonumbers(10,211));
+
+	}
+
+}
