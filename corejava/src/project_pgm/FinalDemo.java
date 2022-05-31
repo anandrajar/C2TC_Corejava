0d61a82a025bb778d3f00dcb70b916f33dc5bd89
aspire <aspire@aspire-PC>
2022-05-31 22:04:53 +0530

    first commit

---
+++ corejava/src/project_pgm/FinalDemo.java
@@ -0,0 +1,18 @@
+package project_pgm;
+
+public class FinalDemo {
+	final int speedlimit=90;//Final variable
+	//void run() {
+		//speedlimit = 50;
+	//}
+//void run gives error cz Final value cannot be changed
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//FinalDemo fd = new FinalDemo();
+				//fd.run();
+
+
+	}
+
+}
