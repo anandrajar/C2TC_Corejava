945ea4c1ac6d7255de7980f2dec211e985b14881
aspire <aspire@aspire-PC>
2022-06-03 15:43:52 +0530

    first commit

---
+++ corejava/src/project_pgm/MrProgram.java
@@ -0,0 +1,14 @@
+package project_pgm;
+
+public class MrProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Character ch = 'a';
+		char a = ch;
+		System.out.println("Character ch: "+ch);
+		System.out.println("char a: "+a);
+
+	}
+
+}
