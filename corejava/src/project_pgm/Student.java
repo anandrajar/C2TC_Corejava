778c390444933ae2726d3c400c61a2471661d5dc
aspire <aspire@aspire-PC>
2022-06-03 15:47:32 +0530

    first commit

---
+++ corejava/src/project_pgm/Student.java
@@ -0,0 +1,15 @@
+package project_pgm;
+//Default Constructor 
+
+public class Student {
+	Student(){
+		System.out.println("Student object is created");
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student ob = new Student();
+
+	}
+
+}
