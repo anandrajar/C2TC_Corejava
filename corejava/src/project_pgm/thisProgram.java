208568c4afab5a0ec769454f0da82950c636384a
aspire <aspire@aspire-PC>
2022-06-03 15:41:32 +0530

    first commit

---
+++ corejava/src/project_pgm/thisProgram.java
@@ -0,0 +1,17 @@
+package project_pgm;
+
+public class thisProgram {
+	int age;
+	thisProgram(int age)
+	{
+		this.age=age;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		thisProgram ob = new thisProgram(21); 
+		System.out.println("Age is "+ob.age);
+
+	}
+
+}
