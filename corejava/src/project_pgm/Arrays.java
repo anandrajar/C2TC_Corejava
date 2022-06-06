dc456268a0ce24296e456ca8321174cb2c986ec7
aspire <aspire@aspire-PC>
2022-06-06 22:20:36 +0530

    first commit

---
+++ corejava/src/project_pgm/Arrays.java
@@ -0,0 +1,13 @@
+package project_pgm;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ar[] = {4,56,88,90};
+		System.out.println("The element in location Two is "+ar[2]);
+	
+
+	}
+
+}
