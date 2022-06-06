28f21a5806ff4c53311ac03832104e0d1c8ce5ab
aspire <aspire@aspire-PC>
2022-06-06 22:23:10 +0530

    first commit

---
+++ corejava/src/project_pgm/ArryaSum.java
@@ -0,0 +1,18 @@
+package project_pgm;
+
+public class ArryaSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[] = {10,20,30,40,50};
+		int sum=0;
+		for(int i=0;i<array.length;i++)
+		{
+			sum+=array[i];
+		}
+		System.out.println("Summ of Array Elements = "+sum);
+
+
+	}
+
+}
