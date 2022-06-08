84d08f47373a477c7e69daaf320acb6d1bd8fde5
aspire <aspire@aspire-PC>
2022-06-08 19:26:40 +0530

    first commit

---
+++ corejava/src/project_pgm/hai.java
@@ -0,0 +1,28 @@
+package project_pgm;
+
+
+interface hello
+{
+	void m1();
+	void m2();
+	
+}
+
+public class hai {
+	void m1()
+	{
+		System.out.println();
+	}
+	void m2()
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hai ob=new hai();
+		ob.m1();
+
+	}
+
+}
