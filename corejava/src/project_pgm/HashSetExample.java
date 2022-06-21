cb0a6605a22c6d8afed872a344ad58ae1a4e4410
aspire <aspire@aspire-PC>
2022-06-21 22:39:09 +0530

    first commit

---
+++ corejava/src/project_pgm/HashSetExample.java
@@ -0,0 +1,19 @@
+package project_pgm;
+
+import java.util.*;
+import java.util.HashSet;
+
+public class HashSetExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedHashSet<String>ins = new LinkedHashSet<String>();
+		HashSet<String> set = new HashSet<String> ();
+		set.add("AR");
+		set.add("RS");
+		set.add("AS");
+	  System.out.println(set);
+
+	}
+
+}
